018c8ce627f047be3717750685b4c6d1b9b9ef5e
suzuki_kiwamu <suzuki_kiwamu@cyberagent.co.jp>
2015-04-08 19:44:04 +0900

    init commit

--- UIScrollView-Sample/ViewController.swift
+++ UIScrollView-Sample/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,21 +23,23 @@ class ViewController: UIViewController {
         let imageView1 = UIImageView(image:img1)
         let imageView2 = UIImageView(image:img2)
         let imageView3 = UIImageView(image:img3)
-        
+        let sampleView = UINib(nibName: "view1", bundle: nil).instantiateWithOwner(self, options: nil)[0] as? view1
         //UIScrollViewを作成します
-        let scrView = UIScrollView()
+//        let scrView = UIScrollView()
         
-        //表示位置 + 1ページ分のサイズ
-        scrView.frame = CGRectMake(50, 50, 240, 240)
-        
-        //全体のサイズ
-        scrView.contentSize = CGSizeMake(240*3, 240)
+//        //表示位置 + 1ページ分のサイズ
+//        scrView.frame = CGRectMake(50, 50, 240, 240)
+//        
+//        //全体のサイズ
+        scrView.contentSize = CGSizeMake(240, 240*4)
         
         //UIImageViewのサイズと位置を決めます
         //左右に並べる
         imageView1.frame = CGRectMake(0, 0, 240, 240)
-        imageView2.frame = CGRectMake(240, 0, 240, 240)
-        imageView3.frame = CGRectMake(480, 0, 240, 240)
+        imageView2.frame = CGRectMake(0, 240, 240, 240)
+        imageView3.frame = CGRectMake(0, 480, 240, 240)
+        sampleView?.frame = CGRectMake(0, 620, 240, 240)
+        
         
         //UIImageViewのサイズと位置を決めます
         //上下に並べる
@@ -49,12 +52,15 @@ class ViewController: UIViewController {
         scrView.addSubview(imageView1)
         scrView.addSubview(imageView2)
         scrView.addSubview(imageView3)
+        scrView.addSubview(sampleView!)
         
         // １ページ単位でスクロールさせる
-        scrView.pagingEnabled = true
+        scrView.pagingEnabled = false
         
         //scroll画面の初期位置
         scrView.contentOffset = CGPointMake(0, 0);
+    
+    	let View = UINib(nibName: "view1", bundle: nil).instantiateWithOwner(self, options: nil)[0] as? view1
     
     }
 
---
+++ UIScrollView-Sample/view1.swift
@@ -0,0 +1,30 @@
+//
+//  view1.swift
+//  UIScrollView-Sample
+//
+//  Created by suzuki_kiwamu on 2015/04/08.
+//  Copyright (c) 2015年 hirokazu maezawa. All rights reserved.
+//
+
+import UIKit
+
+class view1: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+    }
+    
+    
+}
